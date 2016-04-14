321d521136f6f00cc054d78b57c4b7cd28982aaa
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-04-14 09:44:39 -1000

    Clean up unused variables

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TeleOp.java
@@ -3,37 +3,12 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.ftcrobotcontroller.opmodes.util.ArmController;
 import com.qualcomm.ftcrobotcontroller.opmodes.util.DriveMotorController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Servo;
-
 
 public class K9TeleOp extends OpMode {
 
     public DriveMotorController dmc;
 
     public ArmController amc;
-
-    public Gamepad gamepad;
-
-
-    public Servo base_servo,
-                 grip_servo;
-
-    public double base_min = 0.0;
-    public double base_max = 0.5;
-    public double grip_min = 0.0;
-    public double grip_max = 0.9;
-
-    public boolean apressed = false;
-    public boolean bpressed = false;
-
-
-    /*
-        false = back
-        true = forward
-     */
-    public boolean apos = false;
-    public boolean bpos = false;
 
     @Override
     public void loop() {
@@ -50,8 +25,6 @@ public class K9TeleOp extends OpMode {
     @Override
     public void init() {
 
-        //this.gamepad = gamepad1;
-
         dmc = new DriveMotorController(telemetry, hardwareMap.dcMotor.get("left_motor"),
                 hardwareMap.dcMotor.get("right_motor"), gamepad1);
 
@@ -59,10 +32,4 @@ public class K9TeleOp extends OpMode {
 
 
     }
-
-    public void start() {
-        /*base_servo.setPosition(base_min);
-        grip_servo.setPosition(grip_min);*/
-    }
-
 }
